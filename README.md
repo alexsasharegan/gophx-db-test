caa4c5d1c59d468ce33cb7f99aca9e33e6f9babd
Alex Regan <alex.joseph.regan@gmail.com>
2018-10-30 09:41:40 -0700

    feat: implements testing tool

---
+++ README.md
@@ -0,0 +1,45 @@
+# gophx-db-test
+
+A test tool for the Golang Phoenix database meetup in October 2018.
+
+## Test Your Database
+
+To run a validation of your database implementation, first start your
+application so it's listening on the required port. Then go get this test repo,
+move into it's directory, build the binary, and run it:
+
+```sh
+go get github.com/alexsasharegan/gophx-db-test
+cd $GOPATH/src/github.com/alexsasharegan/gophx-db-test
+go build main.go
+
+./main
+```
+
+I've set timeouts in case test commands don't receive their expected input, so
+any deadlocks should fail after ~3 seconds.
+
+## Benchmark Your Database
+
+Results will vary depending on the hardware the client & benchmark is run. There
+are flags to configure the benchmark characteristics:
+
+- `-time` (integer): Sets the duration of the benchmark in seconds _(defaults to
+  10 seconds)_.
+- `-procs` (integer): Sets the number of clients that will interact with the DB
+  during the benchmark _(defaults to 2x number of CPUs)_.
+
+To run a benchmark of your database implementation, first start your application
+so it's listening on the required port. Then go get this test repo, move into
+it's directory, build the binary, and run it:
+
+```sh
+go get github.com/alexsasharegan/gophx-db-test
+cd $GOPATH/src/github.com/alexsasharegan/gophx-db-test
+go build main.go
+
+./main -bench
+
+# or with optional configuration flags
+./main -bench -procs=8 -time=5
+```
---
+++ main.go
@@ -0,0 +1,355 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+	"time"
+)
+
+var (
+	isBenchmark = flag.Bool(
+		"bench", false, "Runs the benchmark instead of the test",
+	)
+	verbose = flag.Bool(
+		"v", false, "Run with extra logging",
+	)
+	numClients = flag.Int(
+		"procs", runtime.NumCPU()*2,
+		"Sets the number of clients that will interact with the DB during the benchmark",
+	)
+	benchTime = flag.Int(
+		"time", 10, "Sets the duration of the benchmark in seconds",
+	)
+)
+
+func main() {
+	if flag.Parse(); *isBenchmark {
+		runBenchmark()
+	} else {
+		runClientTest()
+	}
+}
+
+func runClientTest() {
+	deadline := 3 * time.Second
+	client, err := net.Dial("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("🚫 Failed dialing client: %v\n", err)
+	}
+	defer func() {
+		client.Write([]byte("QUIT\r\n"))
+		client.Close()
+	}()
+	tx := scanLines(client)
+
+	ttCmds := []struct{ input, output []byte }{
+		{[]byte("GET foo\r\n"), []byte("")},
+		{[]byte("SET foo bar\r\n"), []byte("OK")},
+		{[]byte("GET foo\r\n"), []byte("bar")},
+		{[]byte("DEL foo\r\n"), []byte("OK")},
+		{[]byte("GET foo\r\n"), []byte("")},
+		{[]byte("SET foo a value with spaces\r\n"), []byte("OK")},
+		{[]byte("GET foo\r\n"), []byte("a value with spaces")},
+	}
+
+	for i, tc := range ttCmds {
+		if _, err := client.Write(tc.input); err != nil {
+			log.Fatalf(
+				"🚫 Failed command test case[%d] while writing input data: %v\n",
+				i, err,
+			)
+		}
+
+		actual := receiveWithDeadline(tx, deadline)
+		if !bytes.Equal(actual, tc.output) {
+			log.Fatalf(
+				"🚫 Failed command test case[%d]: expected '%s', received '%s'",
+				i, tc.output, actual,
+			)
+		}
+	}
+
+	ttTrans := []struct{ input, output [][]byte }{
+		{
+			input: [][]byte{
+				[]byte("SET foo baz\r\n"),
+				[]byte("GET foo\r\n"),
+				[]byte("DEL foo\r\n"),
+				[]byte("GET foo\r\n"),
+			},
+			output: [][]byte{
+				[]byte("OK"),
+				[]byte("baz"),
+				[]byte("OK"),
+				[]byte(""),
+			},
+		},
+		{
+			input: [][]byte{
+				[]byte("SET x 1\r\n"),
+				[]byte("GET x\r\n"),
+				[]byte("SET x 2\r\n"),
+				[]byte("GET x\r\n"),
+				[]byte("DEL x\r\n"),
+				[]byte("GET x\r\n"),
+			},
+			output: [][]byte{
+				[]byte("OK"),
+				[]byte("1"),
+				[]byte("OK"),
+				[]byte("2"),
+				[]byte("OK"),
+				[]byte(""),
+			},
+		},
+		{
+			input: [][]byte{
+				[]byte("DEL foo\r\n"),
+				[]byte("GET foo\r\n"),
+				[]byte("SET foo 1\r\n"),
+				[]byte("GET foo\r\n"),
+				[]byte("SET foo 2\r\n"),
+				[]byte("GET foo\r\n"),
+				[]byte("DEL foo\r\n"),
+				[]byte("GET foo\r\n"),
+			},
+			output: [][]byte{
+				[]byte("OK"),
+				[]byte(""),
+				[]byte("OK"),
+				[]byte("1"),
+				[]byte("OK"),
+				[]byte("2"),
+				[]byte("OK"),
+				[]byte(""),
+			},
+		},
+	}
+
+	for i, tc := range ttTrans {
+		client.Write([]byte("BEGIN\r\n"))
+		for j, b := range tc.input {
+			if _, err := client.Write(b); err != nil {
+				log.Fatalf(
+					"🚫 Failed transaction test case[%d], command[%d] while writing input data: %v\n",
+					i, j, err,
+				)
+			}
+		}
+		client.Write([]byte("COMMIT\r\n"))
+
+		for j, b := range tc.output {
+			actual := receiveWithDeadline(tx, deadline)
+			if !bytes.Equal(actual, b) {
+				log.Fatalf(
+					"🚫 Failed transaction test case[%d], command[%d]: expected '%s', received '%s'",
+					i, j, b, actual,
+				)
+			}
+		}
+	}
+
+	fmt.Println("✅ All tests passed")
+}
+
+func receiveWithDeadline(tx <-chan []byte, d time.Duration) (b []byte) {
+	select {
+	case <-time.After(d):
+		log.Fatalf("🚫 Failed channel receive with deadline %s", d)
+	case b = <-tx:
+	}
+
+	return b
+}
+
+type benchTestClient struct {
+	conn         net.Conn
+	rx           <-chan []byte
+	transactions []benchTransaction
+	count        int
+}
+
+type benchTransaction struct {
+	data  []byte
+	count int
+}
+
+func runBenchmark() {
+	concurrency := *numClients
+	benchDuration := time.Second * time.Duration(*benchTime)
+	clients := make([]*benchTestClient, concurrency)
+	if !*verbose {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+
+	log.Println(
+		fmt.Sprintf("Connecting %d benchmark test clients.", concurrency),
+	)
+	for i := 0; i < concurrency; i++ {
+		btc := new(benchTestClient)
+		btc.conn = connectDB()
+		defer btc.conn.Close()
+		btc.rx = scanLines(btc.conn)
+		clients[i] = btc
+	}
+
+	log.Println("Generating benchmark test data.")
+
+	var cities = [...]string{
+		"phoenix",
+		"denver",
+		"portland",
+		"seattle",
+		"omaha",
+		"nashville",
+		"flagstaff",
+		"dallas",
+		"austin",
+		"houston",
+		"tokyo",
+		"paris",
+		"santiago",
+		"bogota",
+	}
+
+	for i := 0; i < concurrency; i++ {
+		char := fmt.Sprintf("%c", 'a'+i)
+		city := cities[i%len(cities)]
+		btc := clients[i]
+		btc.transactions = []benchTransaction{
+			{[]byte(fmt.Sprintf("SET %s client[%d]\r\n", char, i)), 1},
+			{[]byte(fmt.Sprintf("GET %s\r\n", char)), 1},
+			{[]byte(fmt.Sprintf("DEL %s\r\n", char)), 1},
+			{
+				bytes.Join(
+					[][]byte{
+						[]byte("BEGIN\r\n"),
+						[]byte(fmt.Sprintf("SET city %s\r\n", city)),
+						[]byte("GET city\r\n"),
+						[]byte("COMMIT\r\n"),
+					},
+					nil,
+				),
+				3,
+			},
+			{[]byte(fmt.Sprintf("SET client %d\r\n", i)), 1},
+			{[]byte("GET client\r\n"), 1},
+			{[]byte("DEL client\r\n"), 1},
+			{
+				bytes.Join(
+					[][]byte{
+						[]byte("BEGIN\r\n"),
+						[]byte("DEL city\r\n"),
+						[]byte("GET city\r\n"),
+						[]byte("COMMIT\r\n"),
+					},
+					nil,
+				),
+				2,
+			},
+			{[]byte("SET city nowhere\r\n"), 1},
+		}
+	}
+
+	go func() {
+		<-time.After(benchDuration + time.Second)
+		log.Fatalln("Benchmark stalled")
+	}()
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(benchDuration))
+	defer cancel()
+
+	for _, btc := range clients {
+		go func(btc *benchTestClient) {
+			for {
+				for _, t := range btc.transactions {
+					select {
+					case <-ctx.Done():
+						return
+					default:
+						btc.conn.Write(t.data)
+						for i := 0; i < t.count; i++ {
+							<-btc.rx
+						}
+						btc.count += t.count
+					}
+				}
+			}
+		}(btc)
+	}
+
+	log.Println(fmt.Sprintf("Running benchmark for %s", benchDuration))
+	<-ctx.Done()
+	log.Println("Benchmark complete.")
+
+	var totalMessages int
+	for _, btc := range clients {
+		totalMessages += btc.count
+	}
+
+	p := message.NewPrinter(language.AmericanEnglish)
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', tabwriter.AlignRight|tabwriter.Debug)
+	defer w.Flush()
+
+	results := [][]string{
+		{"Clients", "Duration", "Messages Sent & Received"},
+		{strconv.Itoa(concurrency), benchDuration.String(), p.Sprintf("%d", totalMessages)},
+	}
+
+	for _, r := range results {
+		fmt.Fprintln(w, strings.Join(r, "\t")+"\t")
+	}
+}
+
+func connectDB() net.Conn {
+	conn, err := net.Dial("tcp", ":8888")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return conn
+}
+
+func scanLines(conn net.Conn) <-chan []byte {
+	tx := make(chan []byte)
+
+	go func() {
+		rd := bufio.NewScanner(conn)
+		for rd.Scan() {
+			tx <- rd.Bytes()
+		}
+	}()
+
+	return tx
+}
+
+// ScanCRLF is adapted from bufio/scan.go
+func ScanCRLF(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.Index(data, []byte{'\r', '\n'}); i >= 0 {
+		// We have a full newline-terminated line.
+		return i + 2, data[0:i], nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
